dec79200b7b5f6fdcd098bbb2b7bba18c9bf208c
mharb <mohamed.harb@cegedim.com>
2023-10-04 11:58:34 +0300

    added manifest

--- set-theory/lakefile.lean
+++ set-theory/lakefile.lean
@@ -8,7 +8,7 @@ package «set-theory» {
 require mathlib from git
   "https://github.com/leanprover-community/mathlib4.git"
 
-@[defaultTarget]
-lean_lib SetTheory {
-  -- add any library configuration options here
-}
+-- @[defaultTarget]
+-- lean_lib SetTheory {
+--   -- add any library configuration options here
+-- }
