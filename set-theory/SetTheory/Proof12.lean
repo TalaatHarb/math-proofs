4a1d9ac79f489f655dd24e926fad0078b0dca2e4
mharb <mohamed.harb@cegedim.com>
2023-11-22 18:28:00 +0200

    added proof12

---
+++ set-theory/SetTheory/Proof12.lean
@@ -0,0 +1,21 @@
+import SetTheory
+open Set
+
+theorem cross_distribute_inter (A B C: Set α ) : cross A (B ∩ C) = (A × B) ∩ (A × C):= by
+  apply Set.ext
+  intro p
+  constructor
+  . intro habc
+    constructor
+    . constructor
+      . exact habc.left
+      . exact And.left habc.right
+    . constructor
+      . exact habc.left
+      . exact And.right habc.right
+  . intro habc
+    constructor
+    . exact (And.left habc).left
+    . constructor
+      . exact habc.left.right
+      . exact habc.right.right
