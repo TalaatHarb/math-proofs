ddef0d37270d40f0869c0af89bbc0c75c8b1353b
mharb <mohamed.harb@cegedim.com>
2023-12-01 23:26:49 +0200

    completed odd and even proofs

--- goldbach/Goldbach/GoldbachProofs.lean
+++ goldbach/Goldbach/GoldbachProofs.lean
@@ -39,19 +39,28 @@ theorem mod_mod (a n : Nat) (hn: n ≠ zero) : (a % n) % n = a % n := by
   apply mod_lt
   exact nat_gt_zero hn
 
-theorem mul_two_even (a: Nat) : even (a * 2):= by
+theorem mul_two_even (k: Nat) : even (k * 2):= by
   unfold even divides
-  induction a with
+  induction k with
   | zero => rfl
-  | succ a ih =>
-  rw [succ_eq_add_one, Nat.add_mul, Nat.one_mul]
-  rw [mod_eq_sub_mod]
+  | succ k ih =>
+  rw [succ_eq_add_one, Nat.add_mul, Nat.one_mul, mod_eq_sub_mod]
   exact ih
   apply le_add_left
 
-theorem two_mul_even (a: Nat) : even (2 * a):= by
+theorem two_mul_even (k: Nat) : even (2 * k):= by
   rw [Nat.mul_comm]
-  exact mul_two_even a
+  exact mul_two_even k
+
+theorem not_mul_two_odd (k: Nat) : odd (2 * k + 1):= by
+  unfold odd divides
+  induction k with
+  | zero => rw [Nat.mul_zero, Nat.zero_add]; trivial
+  | succ k ih =>
+  have he: 1 + 1 = 2 * 1 := by trivial
+  rw [Nat.mul_succ, Nat.succ_add, succ_eq_add_one, Nat.add_assoc, Nat.add_comm 2 _, ← Nat.add_assoc, mod_eq_sub_mod]
+  exact ih
+  apply le_add_left
 
 theorem powers_of_2_even (n: Nat) (h: n ≠ zero): even (2 ^ n):= by
   cases n with
@@ -60,47 +69,166 @@ theorem powers_of_2_even (n: Nat) (h: n ≠ zero): even (2 ^ n):= by
   rw [pow_succ]
   exact mul_two_even (2 ^ n)
 
-theorem even_is_mul_two {a: Nat} (ha: even a) : ∃ k : Nat, a = 2 * k:= by
+theorem non_mul_two_odd (a: Nat) : odd (2 * a + 1):= by
+  unfold odd divides
+  intro h
+  induction a with
+  | zero => rw [Nat.mul_zero, Nat.zero_add] at h; contradiction
+  | succ a ih =>
+  rw [succ_eq_add_one, Nat.mul_add, Nat.mul_one, Nat.add_assoc, Nat.add_comm 2 1, ← Nat.add_assoc, mod_eq_sub_mod] at h
+  exact ih h
+  apply le_add_left
 
-  sorry
+theorem odd_not_even {a: Nat} : odd a ↔ ¬even a:= by
+  constructor
+  . intro h
+    exact h
+  . intro h
+    exact h
+
+theorem not_not {P: Prop} : ¬ ¬ P → P:= by
+  intro h
+  by_cases hp: P
+  . exact hp
+  . contradiction
+
+theorem even_not_odd {a: Nat} : even a ↔ ¬odd a:= by
+  constructor
+  . intro he ho
+    contradiction
+  . intro h
+    exact not_not h
+
+theorem nat_odd_or_even {a: Nat} : odd a ∨ even a := by
+  by_cases h: even a
+  . exact Or.inr h
+  . exact Or.inl h
+
+theorem nat_pattern (a: Nat) : (∃ k: Nat, a = 2 * k) ∨ (∃ k: Nat, a = 2 * k + 1):= by
+  induction a with
+  | zero => apply Or.inl; exists zero;
+  | succ a ih =>
+  cases ih with
+  | inl h =>
+  {
+    cases h with
+    | intro d h =>
+    rw [h]
+    apply Or.inr
+    exists d
+  }
+  | inr h =>
+  {
+    cases h with
+    | intro d h =>
+    rw [h]
+    apply Or.inl
+    exists (d+1)
+  }
+
+theorem even_is_mul_two {a: Nat} (ha: even a) : ∃ k : Nat, a = 2 * k:= by
+  have h:= nat_pattern a
+  cases h with
+  | inl hk => assumption
+  | inr hk =>
+  {
+    cases hk with
+    | intro k hk =>
+    have hc := not_mul_two_odd k
+    rw [hk.symm] at hc
+    contradiction
+  }
+
 
 theorem odd_not_mul_two {a: Nat} (ha: odd a) : ¬ ∃ k : Nat, a = 2 * k:= by
+  have h:= nat_pattern a
+  cases h with
+  | inl hk =>
+  {
+    cases hk with
+    | intro k hk =>
+    have hc:= two_mul_even k
+    rw [hk.symm] at hc
+    contradiction
+  }
+  | inr hk =>
+  {
+    cases hk with
+    | intro k hk =>
+    intro h
+    cases h with
+    | intro d hd =>
+    have hc:= two_mul_even d
+    rw [hd.symm] at hc
+    contradiction
+  }
 
-  sorry
+theorem odd_pattern {a: Nat} (ha: odd a) : ∃ k : Nat, a = 2 * k + 1:= by
+  have h:= nat_pattern a
+  cases h with
+  | inr _ => assumption
+  | inl h =>
+  cases h with
+  | intro k hk =>
+  have hc:= two_mul_even k
+  rw [hk.symm] at hc
+  contradiction
 
 theorem succ_even {a : Nat}(ha: even a): odd (succ a):= by
   have h:= even_is_mul_two ha
-
-  sorry
-
-theorem odd_pattern {a: Nat} (ha: odd a) : ∃ k : Nat, a = 2 * k + 1:= by
-
-  sorry
+  cases h with
+  | intro k hk =>
+  rw [hk, succ_eq_add_one]
+  exact non_mul_two_odd k
 
 theorem succ_odd {a : Nat}(ha: odd a): even (succ a):= by
-  sorry
+  have h:= odd_pattern ha
+  cases h with
+  | intro k hk =>
+  have he: 1 + 1 = 2 * 1:= by trivial
+  rw [hk, succ_eq_add_one, Nat.add_assoc, he, ← Nat.mul_add]
+  exact two_mul_even (k+1)
 
 theorem even_plus_even {a b: Nat} (h: even a ∧ even b): even (a + b):= by
-  have ha:= h.left
-  have hb:= h.right
-
-  sorry
+  have ha:= even_is_mul_two h.left
+  have hb:= even_is_mul_two h.right
+  cases ha with
+  | intro c hc =>
+  cases hb with
+  | intro d hd =>
+  rw [hc, hd, ← Nat.mul_add]
+  exact two_mul_even (c+d)
 
 theorem even_plus_odd {a b: Nat} (h: even a ∧ odd b): odd (a + b):= by
-  have ha:= h.left
-  have hb:= h.right
-
-  sorry
+  have ha:= even_is_mul_two h.left
+  have hb:= odd_pattern h.right
+  cases ha with
+  | intro c hc =>
+  cases hb with
+  | intro d hd =>
+  rw [hc, hd, Nat.add_comm, Nat.add_assoc, Nat.add_left_comm, ← Nat.mul_add, Nat.add_comm]
+  exact not_mul_two_odd (d+c)
 
 theorem odd_plus_odd {a b: Nat} (h: odd a ∧ odd b): even (a + b):= by
-  have ha:= h.left
-  have hb:= h.right
-
-  sorry
+  have ha:= odd_pattern h.left
+  have hb:= odd_pattern h.right
+  cases ha with
+  | intro c hc =>
+  cases hb with
+  | intro d hd =>
+  have he: 1 + 1 = 2 * 1:= by trivial
+  rw [hc, hd, Nat.add_left_comm, Nat.add_assoc, he, ← Nat.mul_add, ← Nat.mul_add]
+  exact two_mul_even (d + (c + 1))
 
 theorem odd_plus_even {a b: Nat} (h: odd a ∧ even b): odd (a + b):= by
-
-  sorry
+  have ha:= odd_pattern h.left
+  have hb:= even_is_mul_two h.right
+  cases ha with
+  | intro c hc =>
+  cases hb with
+  | intro d hd =>
+  rw [hc, hd, Nat.add_assoc, Nat.add_left_comm, ←Nat.mul_add, Nat.add_comm]
+  exact not_mul_two_odd (c+d)
 
 theorem goldbach_conjuctrue (n: Nat)(he: even n)(h2: n ≥ 2) : ∃ a b: Nat, (n = a + b) ∧ (prime a) ∧ (prime b) := by
   -- no one knows how to solve this
